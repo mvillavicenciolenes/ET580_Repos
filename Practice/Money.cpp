8cda6c79648226cfbda877e6c9f7b7d5ecd7cdfd
mvillavicenciolenes <143456253+mvillavicenciolenes@users.noreply.github.com>
2024-11-04 23:57:53 +0000

    made changes

--- Practice/Money.cpp
+++ Practice/Money.cpp
@@ -1,5 +1,53 @@
 // Money.cpp
-
 #include "Money.h"
+#include <iostream>
 
-// Empty implementation file since all functionality is defined inline in Money.h
+// Constructors
+Money::Money() : dollars(0) {}               // Default constructor
+Money::Money(int d) : dollars(d) {}          // Parameterized constructor
+
+// Accessor and Mutator
+int Money::getMoney() const {
+    return dollars;
+}
+
+void Money::setMoney(int d) {
+    dollars = d;
+}
+
+// Overloaded operators
+Money Money::operator-() const {             // Unary minus
+    return Money(-dollars);
+}
+
+Money& Money::operator+=(const Money& rhs) { // += operator
+    dollars += rhs.dollars;
+    return *this;
+}
+
+Money Money::operator+(const Money& rhs) const { // + operator
+    return Money(dollars + rhs.dollars);
+}
+
+bool Money::operator==(const Money& rhs) const { // == operator
+    return dollars == rhs.dollars;
+}
+
+// Prefix increment operator
+Money& Money::operator++() {
+    ++dollars;
+    return *this;
+}
+
+// Postfix increment operator
+Money Money::operator++(int) {
+    Money temp = *this;
+    ++(*this);
+    return temp;
+}
+
+// Stream insertion operator for printing
+std::ostream& operator<<(std::ostream& os, const Money& m) {
+    os << "$" << m.dollars;
+    return os;
+}
--- Practice/Money.h
+++ Practice/Money.h
@@ -11,45 +11,25 @@ private:
 
 public:
     // Constructors
-    Money() : dollars(0) {}                      // Default constructor
-    Money(int d) : dollars(d) {}                 // Parameterized constructor
+    Money();                   // Default constructor
+    Money(int d);              // Parameterized constructor
 
-    // Accessor for getting dollars value
-    int getMoney() const { return dollars; }
-    
+    // Accessor and Mutator
+    int getMoney() const;      // Getter for dollars
+    void setMoney(int d);      // Setter for dollars
+
     // Overloaded operators
-    Money operator-() const { return Money(-dollars); }  // Unary minus
-
-    Money& operator+=(const Money& rhs) {                // += operator
-        dollars += rhs.dollars;
-        return *this;
-    }
-
-    Money operator+(const Money& rhs) const {            // + operator
-        return Money(dollars + rhs.dollars);
-    }
-
-    bool operator==(const Money& rhs) const {            // == operator
-        return dollars == rhs.dollars;
-    }
-
-    // Stream insertion operator for printing
-    friend std::ostream& operator<<(std::ostream& os, const Money& m) {
-        os << "$" << m.dollars;
-        return os;
-    }
+    Money operator-() const;                  // Unary minus
+    Money& operator+=(const Money& rhs);      // += operator
+    Money operator+(const Money& rhs) const;  // + operator
+    bool operator==(const Money& rhs) const;  // == operator
 
     // Prefix and postfix increment operators
-    Money& operator++() {        // Prefix ++
-        ++dollars;
-        return *this;
-    }
+    Money& operator++();                      // Prefix ++
+    Money operator++(int);                    // Postfix ++
 
-    Money operator++(int) {      // Postfix ++
-        Money temp = *this;
-        ++(*this);
-        return temp;
-    }
+    // Stream insertion operator for printing
+    friend std::ostream& operator<<(std::ostream& os, const Money& m);
 };
 
 #endif
